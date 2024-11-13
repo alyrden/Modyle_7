69afa642979eb0dbb0d5c69cfa70493fd8ee6619
alyrden <f50knox@gmail.com>
2024-11-13 23:03:36 +0300

    7

--- Modyle_7.1.py
+++ Modyle_7.1.py
@@ -1,49 +1,49 @@
-from pprint import pprint
-
-
+# from importlib.resources import files
+# from pprint import pprint
+#
+#
 class Product:
 
-  def __init__(self, name, weigth, category):
-      self.name = name
-      self.weigth = weigth
-      self.category = category
+    def __init__(self, name, weigth, category):
+        self.name = name
+        self.weigth = weigth
+        self.category = category
 
-  def __str__(self):
-      return f"{self.name}, {self.weigth}, {self.category}"
-
+    def __str__(self):
+        return f"{self.name}, {self.weigth}, {self.category}"
+#
+#
 class Shop:
 
     def __init__(self):
         self.__file_name = 'products.txt'
 
     def get_products(self):
-        file = open(self.__file_name, 'r', encoding='utf-8')
-        file.read()
-        file.close()
-        # return file.read()
-
+        file_all = open(self.__file_name, 'r', encoding='utf-8')
+        return file_all.read()
 
     def add(self, *products):
-        existing_products = set()
-        file_name = self.get_products().strip().splitlines()
 
-        for line in file_name:
+        existing_products = set()
+
+        file_a = self.get_products().strip().splitlines()
+
+        for line in file_a:
             if line:
                 name = line.split(', ')[0]
                 existing_products.add(name)
-        file_all = open(self.__file_name, 'a', encoding='utf-8')
+                print(file_a)
 
-        # # file_all.close()
-        file_all = open(self.__file_name, 'a', encoding='utf-8')
+        file_al = open(self.__file_name, 'a', encoding='utf-8')
+
         for product in products:
             if product.name not in existing_products:
-                file_all.write(f"{product}\n")
-                existing_products.add(product.name)
-                # file_all.close()
+                    file_al.write(f"{product}\n")
+                    existing_products.add(product.name)
             else:
-                print(f"Продукт {product.name} уже есть в магазине")
+                    print(f"Продукт {product.name} уже есть в магазине")
 
-        file_all.close()
+
 
 s1 = Shop()
 p1 = Product('Potato', 50.5, 'Vegetables')
@@ -55,3 +55,5 @@ print(p2)  # __str__
 s1.add(p1, p2, p3)
 
 print(s1.get_products())
+
+
